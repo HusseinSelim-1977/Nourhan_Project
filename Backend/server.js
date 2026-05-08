5f29488444548158c7c20aa9632846c8d5491e1f
HusseinSelim-1977 <husseinsherif2006@gmail.com>
2026-05-08 15:00:44 +0300

    Finalizing project

---
+++ Backend/server.js
@@ -0,0 +1,11 @@
+const app = require('./src/app');
+require('dotenv').config();
+const connectDB = require('./src/config/db');
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`GIU Nexus API running on port ${PORT} [${process.env.NODE_ENV}]`);
+  });
+});
